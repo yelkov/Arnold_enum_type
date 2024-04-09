0da215dfdd3f428da917be04e0bf32076ef3a03b
mmuina98 <102675254+mmuina98@users.noreply.github.com>
2024-04-09 13:12:29 +0200

    feat(Planeta.java): add planets

---
+++ app/src/main/java/edu.badpals.arnold/Planeta.java
@@ -0,0 +1,24 @@
+package edu.badpals.arnold;
+
+public enum Planeta {
+
+    MERCURY (3.303e+23, 2.4397e6),
+    VENUS   (4.869e+24, 6.0518e6),
+    EARTH   (5.976e+24, 6.37814e6),
+    MARS    (6.421e+23, 3.3972e6),
+    JUPITER (1.9e+27,   7.1492e7),
+    SATURN  (5.688e+26, 6.0268e7),
+    URANUS  (8.686e+25, 2.5559e7),
+    NEPTUNE (1.024e+26, 2.4746e7);
+
+    private Double masa = 0d;
+    private Double radio = 0d;
+
+    private Planeta(Double masa, Double radio) {
+        this.masa = masa;
+        this.radio = radio;
+    }
+
+
+}
+
---
+++ app/src/test/java/edu.badpals.arnold/ArnoldEnumTypeTest.java
@@ -0,0 +1,107 @@
+package edu.badpals.arnold;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ArnoldEnumTypeTest {
+
+	public static String[] planetas;
+
+	@BeforeClass
+	public static void CreacionArrayPlanetasSetup() {
+		planetas = new String[8];
+		int planetasIncluidos = 0;
+		for (Planeta planeta : Planeta.values()) {
+			planetas[planeta.ordinal()] = planeta.name();
+			planetasIncluidos += 1;
+		}
+		assertThat(planetasIncluidos).isEqualTo(Planeta.values().length);
+		assertThat(planetas).doesNotContainNull();
+	}
+
+	@Test
+	public void PlanetaConstructorTest() {
+		// voy a asegurarme de que los metodos de los Enum Types
+		// se comportan como yo espero
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta).isInstanceOf(Planeta.class);
+		assertThat(planeta.ordinal()).isZero();
+		assertThat(planeta.name()).isEqualToIgnoringCase("MERCURY");
+		assertThat(Planeta.valueOf(planeta.name())).isEqualTo(Planeta.MERCURY);
+		assertThat(planeta.compareTo(Planeta.MERCURY)).isZero();
+		assertThat(planeta.toString()).isEqualToIgnoringCase("MERCURY");
+		assertThat(planeta.equals(Planeta.MERCURY)).isTrue();
+		assertThat(Planeta.values()[0]).isEqualTo(planeta);
+	}
+
+	@Test
+	public void PlanetaGetMasaTest() {
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta.getMasa()).isEqualTo(3.303e+23);
+	}
+
+/*
+	@Test
+	public void PlanetaGetRadioTest() {
+		Planeta planeta = Planeta.MERCURY;
+		assertThat(planeta.getRadio()).isEqualTo(2.4397e+6);
+	}
+*/
+/*
+
+	@Test
+	public void PlanetaNamesIteratorTest() {
+		for (Planeta planeta : Planeta.values()) {
+			assertThat(planeta.name()).isIn(planetas);
+		}
+	}
+*/
+
+/*
+	@Test
+	public void PesoSuperficieMercurioTest() {
+		Planeta planeta = Planeta.MERCURY;
+		double pesoHumano = 175;
+		assertEquals(66.107583, planeta.pesoSuperficie(pesoHumano), 0.001);
+	}
+*/
+/*	@Test
+	public void ArrayPlanetasTerrestresTest() {
+
+		String[] planetasTerrestres = new String[4];
+		int planetasIncluidos = 0;
+
+		for (int i = Planeta.MERCURY.ordinal(); i < Planeta.JUPITER.ordinal(); i++) {
+			planetasTerrestres[i] = Planeta.values()[i].name();
+			planetasIncluidos += 1;
+		}
+		assertThat(planetasIncluidos).isEqualTo(4);
+		assertThat(planetas).doesNotContainNull();
+
+		for (Planeta planeta : Planeta.getPlanetasTerrestres()) {
+			assertThat(planeta.name()).isIn(planetasTerrestres);
+		}
+	}*/
+
+/*	@Test
+	public void ArrayGigantesGaseosos() {
+
+		String[] gigantesGaseosos = new String[4];
+		int planetasIncluidos = 0;
+
+		byte index = 0;
+		for (int i = Planeta.JUPITER.ordinal(); i <= Planeta.NEPTUNE.ordinal(); i++) {
+			gigantesGaseosos[index] = Planeta.values()[i].name();
+			planetasIncluidos += 1;
+			index += 1;
+		}
+		assertThat(planetasIncluidos).isEqualTo(4);
+		assertThat(planetas).doesNotContainNull();
+
+		for (Planeta planeta : Planeta.getGigantesGaseosos()) {
+			assertThat(planeta.name()).isIn(gigantesGaseosos);
+		}
+	}*/
+}
